c2761729f36c1cde40cbeff64759a4ad469ae9ce
Kris <kristina.yaruta@gmail.com>
2016-02-16 17:55:38 +0200

    lesson3 - создание класса point

---
+++ snake/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            point p1 = new point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+
+            point p2 = new point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
+   
+
+            Console.ReadLine();
+        }
+        
+    }
+}
