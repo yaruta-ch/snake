707799640a9304e8392b91077e45b40663aa93a3
Kris <kristina.yaruta@gmail.com>
2016-02-18 12:00:28 +0200

    Создали класс список

--- snake/Program.cs
+++ snake/Program.cs
@@ -15,7 +15,27 @@ namespace snake
 
             point p2 = new point(5,14,'#');
             p2.Draw();
-   
+
+
+            List<char> chList = new List<char>();
+            chList.Add('$');
+            chList.Add('^');
+            chList.Add('&');
+
+            char x = chList[0];
+            char y = chList[1];
+
+            foreach(char i in chList)
+            {
+                Console.WriteLine(i);
+            }
+
+            chList.RemoveAt(1);
+
+            List<point> pList = new List<point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
             Console.ReadLine();
         }
         
